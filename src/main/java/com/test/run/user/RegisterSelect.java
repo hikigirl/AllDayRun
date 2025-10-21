d367420c5a9dfbd8ab6d67a0335985fb6297abf6
홍선민 <sunmin323233@gmail.com>
2025-10-21 22:51:32 +0900

    user패키지 수정

--- src/main/java/com/test/run/user/RegisterSelect.java
+++ src/main/java/com/test/run/user/RegisterSelect.java
@@ -1,10 +1,5 @@
 package com.test.run.user;
 
-/**
- * 사용자 회원가입 유형 선택 페이지를 처리하는 클래스
- * 일반 회원가입 또는 소셜 로그인(예: Google)을 선택하는 페이지를 제공한다.
- */
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -13,19 +8,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/**
+ * 사용자 회원가입 유형 선택 페이지를 처리하는 클래스
+ * 일반 회원가입 또는 소셜 로그인(예: Google)을 선택하는 페이지를 제공한다.
+ */
 @WebServlet(value = "/user/registerselect.do")
 public class RegisterSelect extends HttpServlet {
+	/**
+     * GET 요청을 처리한다. 회원가입 유형 선택 페이지를 클라이언트에게 반환
+     * 
+     * @param req 클라이언트로부터의 HttpServletRequest 객체
+     * @param resp 클라이언트로의 HttpServletResponse 객체
+     * @throws ServletException 서블릿 관련 오류
+     * @throws IOException 입출력 오류
+     */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-	    /**
-	     * GET 요청을 처리한다. 회원가입 유형 선택 페이지를 클라이언트에게 반환
-	     * 
-	     * @param req 클라이언트로부터의 HttpServletRequest 객체
-	     * @param resp 클라이언트로의 HttpServletResponse 객체
-	     * @throws ServletException 서블릿 관련 오류
-	     * @throws IOException 입출력 오류
-	     */
-		// RegisterSelect.java
+		//RegisterSelect.java
 
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/registerselect.jsp");
 		dispatcher.forward(req, resp);
